72211ed844a660121a83c96dd8ed87479e86c29d
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 06:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function moveDodgerLeft() {
   function moveDodgerLeft() {
     let rightNumbers = dodger.style.right.replace("px", "");
     let right = parseInt(rightNumbers, 10);
-   
+
     if (right> 0) {
       dodger.style.right = `${right - 1}px`;
     }
