3cdbef6833c2fc1713aeda518ca72e1ac284b3b4
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 06:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function moveDodgerLeft() {
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
-}
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerRight();
+  function moveDodgerLeft() {
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+   
+    if (right> 0) {
+      dodger.style.right = `${right - 1}px`;
+    }
   }
-});
