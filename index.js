cabebaf81c249c31600cc98087119142eba7540a
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 05:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Your code here
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-});
+}
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerRight();
